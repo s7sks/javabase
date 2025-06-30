8890194c535360e17077610b2fc25680053dd121
Your Name <you@example.com>
2025-07-01 00:17:36 +0800

    修改1

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
             System.out.println("i = " + i);
