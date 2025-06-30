a7beb54606104a954915f041ff1d62ba8df960c8
Your Name <you@example.com>
2025-07-01 00:25:47 +0800

    修改2

---
+++ src/day01/Demo01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
