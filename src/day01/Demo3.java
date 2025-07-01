787749687189ecfd16341c43ed376695217ee0bb
Your Name <you@example.com>
2025-07-01 15:02:25 +0800

    九九乘法表

---
+++ src/day01/Demo3.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        //打印九九乘法表
+        int i=0,j=0;
+        for (i=1;i<=9;i++){
+            for (j=1;j<=9;j++){
+                if (j>i) break;
+                System.out.println(j+"×"+i+"="+(i*j)+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
