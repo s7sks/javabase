a438fbf558108a612d1eca4facc5fad821c405fb
Your Name <you@example.com>
2025-07-01 14:20:30 +0800

    while dowhile循环学习

---
+++ src/day01/Demo02.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        int i=100;
+        while (i>0){
+            System.out.println("hello");
+            i=i/2;
+        }
+
+        int j=10;
+        do{
+            System.out.println(j);
+            j--;
+        }while (j>0);  //dowhile:不论条件满不满足都会先执行一次循环内容
+    }
+}
